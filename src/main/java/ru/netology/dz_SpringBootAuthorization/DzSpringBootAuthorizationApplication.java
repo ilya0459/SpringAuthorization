0e30d349d3569fa3eb0d514e2a80c307aaeba3f5
Dmitrii_Trebin <d.trebin@rambler.ru>
2024-08-08 13:54:22 +0300

    Edit class AuthorizationController

--- src/main/java/ru/netology/dz_SpringBootAuthorization/DzSpringBootAuthorizationApplication.java
+++ src/main/java/ru/netology/dz_SpringBootAuthorization/DzSpringBootAuthorizationApplication.java
@@ -2,6 +2,9 @@ package ru.netology.dz_SpringBootAuthorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.netology.dz_SpringBootAuthorization.controller.AuthorizationController;
+import ru.netology.dz_SpringBootAuthorization.repository.UserRepository;
+import ru.netology.dz_SpringBootAuthorization.service.AuthorizationService;
 
 @SpringBootApplication
 public class DzSpringBootAuthorizationApplication {
--- src/main/java/ru/netology/dz_SpringBootAuthorization/controller/AuthorizationController.java
+++ src/main/java/ru/netology/dz_SpringBootAuthorization/controller/AuthorizationController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.dz_SpringBootAuthorization.model.Authorities;
+import ru.netology.dz_SpringBootAuthorization.repository.UserRepository;
 import ru.netology.dz_SpringBootAuthorization.service.AuthorizationService;
 
 import java.util.List;
@@ -11,6 +12,9 @@ import java.util.List;
 @RestController
 public class AuthorizationController {
     AuthorizationService service;
+    public AuthorizationController(AuthorizationService service) {
+        this.service = service;
+    }
 
     @GetMapping("/authorize")
     public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
